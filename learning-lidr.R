d3ac1a46d844bdc809aa454de0687f9806874513
Don De Alban <dondealban@gmail.com>
2022-01-26 14:00:24 +0800

    Initial commit

---
+++ learning-lidr.R
@@ -0,0 +1,33 @@
+# Learning lidR R package for Airborne LIDAR Data Manipulation and Visualisation
+# for Forestry Applications
+# 
+# Script by:      Jose Don De Alban
+# Date created:   25 Jan 2022
+# Date modified:  
+
+# Load libraries
+library(lidR)
+
+# Set working directory
+pathPD <- ("/Users/dondealban/Dropbox/Image Processing/Indonesia/APRIL RER/LIDAR Data/pulau padang/")
+setwd(pathPD)
+
+# Load files
+tile_217_139 <- readLAS("217_139.las")
+plot(tile_217_139)    # plot lidar data
+print(tile_217_139)   # print lidar data summary
+summary(tile_217_139) # print lidar data detailed summary
+
+# Load selected attributes only: using select parameter
+tile_xyz  <- readLAS("217_139.las", select = "xyz")  # load XYZ only
+plot(tile_xyz)
+print(tile_xyz)
+
+tile_xyzi <- readLAS("217_139.las", select = "xyzi") # load XYZ and intensity only
+plot(tile_xyzi)
+print(tile_xyzi)
+
+# Load selected attributes only: using filter parameter
+tile_1ret <- readLAS("217_139.las", filter = "-keep_first") # Read only first returns
+plot(tile_1ret)
+print(tile_1ret)
