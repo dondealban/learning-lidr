eb3ec68bb10523e8916afe3074c0d1d299e82f50
Don De Alban <dondealban@gmail.com>
2022-02-03 17:52:18 +0800

    Added lines for classifying ground using pmf

--- learning-lidr-ch03.R
+++ learning-lidr-ch03.R
@@ -17,13 +17,36 @@ setwd(pathPD)
 
 # 3.1. Progressive Morphological Filter -----------
 
-# Load files
+# Lidar data test
+LASfile <- system.file("extdata", "Topography.laz", package="lidR")
+lasTest <- readLAS(LASfile, select = "xyzrn")
+plot(lasTest)    # plot lidar data
+print(lasTest)   # print lidar data summary
+summary(lasTest) # print lidar data detailed summary
+lasTest <- classify_ground(lasTest, algorithm = pmf(ws = 5, th = 3))
+plot(lasTest, color = "Classification", size = 3, bg = "white") 
+
+p1 <- c(273420, 5274455)
+p2 <- c(273570, 5274460)
+plot_crossection(las, p1 , p2, colour_by = factor(Classification))
+
+
+
+# Lidar data: Pulau Padang
 tile <- readLAS("217_139.las")
 plot(tile)    # plot lidar data
 print(tile)   # print lidar data summary
 summary(tile) # print lidar data detailed summary
-
-
+plot(tile, color = "Classification", size = 3, bg = "white") 
+# Without filtering
+  p1 <- c(217300, 139100)
+  p2 <- c(217400, 139000)
+  plot_crossection(tile, p1 , p2, colour_by = factor(Classification))
+# With filtering steps
+  ws <- seq(3, 12, 3)
+  th <- seq(0.1, 1.5, length.out = length(ws))
+  tileNew <- classify_ground(tile, algorithm = pmf(ws = ws, th = th))
+  plot_crossection(tileNew, p1 = p1, p2 = p2, colour_by = factor(Classification))
 
 
 # 3.2. Cloth Simulation Function ------------------
